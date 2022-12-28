04634b661a8dfa559f6bb9b183fc08955e6fa264
yakirarage <yaranaafs@gmail.com>
2022-12-28 18:55:49 +0100

    added utility.utils

--- cogs/events.py
+++ cogs/events.py
@@ -11,5 +11,13 @@ class Events(commands.Cog):
 		print(f"id: {self.bot.user.id}")
 		print(f"guilds: {len(self.bot.guilds)}")
 
+	@commands.Cog.listener()
+	async def on_guild_join(self, guild):
+		print(f"➕ Joined \"{guild.name}\"")
+		for channel in guild.text_channels:
+			if channel.permissions_for(guild.me).send_messages:
+				await channel.send("Hello!, Thanks for adding me! You can use the **/help** To learn how the bot works!")
+				break
+
 def setup(bot):
 	bot.add_cog(Events(bot))
---
+++ utility/utils.py
@@ -0,0 +1,27 @@
+class ConsoleColors:
+    HEADER  = '\033[95m'
+    BLUE    = '\033[94m'
+    CYAN    = '\033[96m'
+    GREEN   = '\033[92m'
+    YELLOW  = '\033[1;33m'
+    LRED    = '\033[1;31m'
+    WARNING = '\033[93m'
+    FAIL    = '\033[91m'
+    ENDC    = '\033[0m'
+    BOLD    = '\033[1m'
+    UNDER   = '\033[4m'
+
+async def create_player_info(inter, mem):
+    dat = await inter.bot.players.find_one({"_id": mem.id})
+    if dat is None:
+        new_account = {
+            # unique idx
+            "_id": inter.guild.id,
+
+            #channel id's
+            "logs_chan": None
+        }
+
+        await inter.bot.players.insert_one(new_account)
+    else:
+        return
